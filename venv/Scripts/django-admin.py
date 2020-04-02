c14749cab1e8558aab7ba671dedc5fd392a63770
knkemree <konakziyaemre@gmail.com>
2020-04-02 11:50:05 -0400

    initial

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\prime\OneDrive\Desktop\Brain's Project\brian_django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
